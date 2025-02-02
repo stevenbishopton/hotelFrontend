8a4d5d8b9de4d6af540feba2ee69bde66a92d978
stevenbishopton <142553264+stevenbishopton@users.noreply.github.com>
2025-02-01 23:26:04 -0800

    updated an import issue

--- src/CreateRoom.jsx
+++ src/CreateRoom.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './CreateRoom.css';
+import '.createRoom.css';
 
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
